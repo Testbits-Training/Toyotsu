2a8fa6f92e061f49535ff108fd4928ffec2095e4
Azwan Azlan <azwanazlan2809@gmail.com>
2022-11-16 17:47:21 +0800

    test

---
+++ Test Suites/Successful Test Case.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Successful Test Case</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>73d9c826-b21d-482f-9b35-960d0a6f0d78</testSuiteGuid>
+</TestSuiteEntity>
