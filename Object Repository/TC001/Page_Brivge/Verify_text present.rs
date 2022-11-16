a01aa6b39676531b020f0a4a0c1f3cc2229f3d36
Azwan Azlan <azwanazlan2809@gmail.com>
2022-11-16 16:03:54 +0800

    update test

---
+++ Object Repository/TC001/Page_Brivge/Verify_text present.rs
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Verify_text present</name>
+   <tag></tag>
+   <elementGuidId>2ebfd6e9-b7e3-49d8-ad6b-36ae7fc63254</elementGuidId>
+   <imagePath></imagePath>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//*[(text() = 'Azwan has login' or . = 'Azwan has login')]</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value>//*[(text() = 'Has login' or . = 'Has login')]</value>
+      </entry>
+      <entry>
+         <key>IMAGE</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+</WebElementEntity>
